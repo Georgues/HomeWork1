952ee7e4fab443f34b1cb67fcc4fc355ff769f8f
Georgues <geragostev@gmail.com>
2020-12-16 05:19:30 +0300

    Смена кодировки в функции main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <windows.h>
+#include <cstdlib>
+#include <cctype>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    // Смена кодировки консоли
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Дз 1" << endl;
+
     return 0;
 }
+
